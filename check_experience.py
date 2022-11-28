c935d917ecc1ead3c8f3a5c7ea0f4391c1f6a74c
Nireeksha D Rai <76628362+Nireekshadrai@users.noreply.github.com>
2022-11-29 02:10:54 +0530

    Update check_experience.py

--- check_experience.py
+++ check_experience.py
@@ -1,44 +1,42 @@
 def experience(res_dct,condition,field):
     if '>' in condition:
                             if ">=" in condition: 
-                                print("enterered")
                                 ndeg=condition.replace(">=", '').strip()
                                 print(ndeg)
                                 if ndeg.isdigit():
                                     if((res_dct["total_exp"].strip())>=ndeg):
-                                        print("enter2")
                                         return True
                                 else:
-                                    print("enter valid input")
+                                    return "invalid"
                             else:
                                 ndeg=condition.replace('>', '').strip()
                                 if ndeg.isdigit():
                                     if((res_dct["total_exp"].strip())>ndeg):
                                         return True
                                 else:
-                                    print("enter valid input")
+                                    return "invalid"
     elif '<' in condition: 
                             if "<=" in condition: 
-                                ndeg=condition.replace("<=", '').strip()          #string immutable in python
+                                ndeg=condition.replace("<=", '').strip()          
                                 if ndeg.isdigit():
                                     if((res_dct["total_exp"].strip())<=ndeg):
                                         return True
                                 else:
-                                    print("enter valid input")
+                                    return "invalid"
                             else:
                                 ndeg=condition.replace('<', '').strip()
                                 if ndeg.isdigit():
                                     if((res_dct["total_exp"].strip())<ndeg):
                                         return True
                                 else:
-                                    print("enter valid input")
+                                    return "invalid"
     elif '=' in condition: 
                             ndeg=condition.replace('=', '').strip()
                             if ndeg.isdigit():
                                 if((res_dct["total_exp"].strip())==ndeg):
                                     return True
                             else:
-                                print("enter valid input")
+                                return "invalid"
                         
     else:
-        print("invalid input for experience")
+        return "invalid"

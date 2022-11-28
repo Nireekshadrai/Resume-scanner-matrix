438121751926f0610da0ea2e54bd62fb6df860a3
Nireeksha D Rai <76628362+Nireekshadrai@users.noreply.github.com>
2022-11-29 02:13:23 +0530

    Update pdfminer_multiple.py

--- pdfminer_multiple.py
+++ pdfminer_multiple.py
@@ -1,16 +1,9 @@
 import pdfminer_single
-from io import StringIO
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-import PyPDF2
 import os
 import sys, getopt
+import streamlit as st
 
-#requireent- all pdfs and texts in the pdf folder should be resumes... tghats why its better not to
-#requirement-text folder should be empty
-#converts pdf, returns its text content as a string
+x=0
 def convertMult(pdfDir, txtDir):
     for pdf in os.listdir(pdfDir): #iterate through pdfs in pdf directory
             fileExtension = pdf.split(".")[-1]
@@ -18,21 +11,8 @@ def convertMult(pdfDir, txtDir):
                 pdfFilename = pdfDir + pdf
                 text = pdfminer_single.convert(pdfFilename) #get string of text content of pdf
                 textFilename = txtDir + pdf + ".txt"
-                if os.path.exists(textFilename):
-                    while True:
-                        val=input("file " +textFilename +" already exists do you want to overwrite (yes/no)(no-quit)")#testing
-                        if val=="yes":
-                            textFile = open(textFilename, "w") #make text file
-                            textFile.write(text) #write text to text file
-                            break
-                        elif val=="no":
-                            exit()
-                        else:
-                            print("enter either yes or no only")
-                            continue
-                else:
-                    textFile = open(textFilename, "w") #make text file
-                    textFile.write(text) #write text to text file        
+                textFile = open(textFilename, "w") #make text file
+                textFile.write(text) #write text to text file        
         
                         
 def convertMultiple(pdfDir, txtDir):
@@ -40,19 +20,5 @@ def convertMultiple(pdfDir, txtDir):
     if os.path.exists(pdfDir) and os.path.exists(txtDir) :
         convertMult(pdfDir, txtDir)
                 #textFile.close
-    else:
-        if not os.path.exists(txtDir):  #testing
-            while True:
-                val=input("this directory does not exist do you want to create one (yes/no) (no-quit program)")   #testing
-                if val=="yes":
-                    os.mkdir(txtDir)
-                    convertMult(pdfDir, txtDir)
-                    break
-                elif val=="no":
-                    exit()
-                else:
-                    print("enter either yes or no only")
-                    continue
-        else:
-            print("pdf directory of resumes does not exist")
+
 
